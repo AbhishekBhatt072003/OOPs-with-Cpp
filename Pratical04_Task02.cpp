0ee081aecddf3210044a947b5f0706743206942f
Abhishek Bhatt <86089661+AbhishekBhatt072003@users.noreply.github.com>
2021-09-29 10:42:31 +0530

    Add files via upload

---
+++ Pratical04_Task02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "first.h"
+#include "second.h"
+
+using namespace std;
+
+
+int main()
+{
+int a = add(45,78);
+float b = add(454.5, 45.4);
+cout << a <<" " << b;
+
+return 0;
+}
